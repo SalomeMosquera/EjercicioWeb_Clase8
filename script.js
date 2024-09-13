d5b9f16474ec0cb6a6eee496bace3182b8b8622b
SalomeMosquera <salome.mosquera@uao.edu.co>
2024-09-12 21:30:49 -0500

    EjercicioEnClase_PrimeraParte

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello!');
+const evaluar = () => {
+    const num1 = prompt("Ingresa un número positivo");
+    const num2 = prompt("Ingresa un número positivo");
+    const num3 = prompt("Ingresa un número positivo");
+
+    let msj = num1 < num2 && num1 < num3? (document.getElementById("resultado").innerText = `El número menor es ${num1}`) 
+    :num2 < num1 && num2 < num3? (document.getElementById("resultado").innerText = `El número menor es ${num2}`)
+    :(document.getElementById("resultado").innerText = `El número menor es ${num3}`);
+
+    num1 == num2 || num1 == num3 ? (document.getElementById("igual").innerText = `El número que se repite es el: ${num1}`)
+    : num2 == num1 || num2 == num3? (document.getElementById("igual").innerText = `El número que se repite es el: ${num2}`)
+    : num3 == num1 || num3 == num2? (document.getElementById("igual").innerText = `El número que se repite es el: ${num3}`)
+    : (document.getElementById("igual").innerText = `No hay numeros repetidos`);
+       
+
+
+  };
