4016ab30317ea7b8e25a63dedd1c7ec617e095a8
SalomeMosquera <salome.mosquera@uao.edu.co>
2024-09-13 09:56:22 -0500

    Segundo Comit

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const evaluar = () => {
-    const num1 = prompt("Ingresa un número positivo");
-    const num2 = prompt("Ingresa un número positivo");
-    const num3 = prompt("Ingresa un número positivo");
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const num3 = parseFloat(document.getElementById("num3").value);
 
-    let msj = num1 < num2 && num1 < num3? (document.getElementById("resultado").innerText = `El número menor es ${num1}`) 
-    :num2 < num1 && num2 < num3? (document.getElementById("resultado").innerText = `El número menor es ${num2}`)
-    :(document.getElementById("resultado").innerText = `El número menor es ${num3}`);
+    let msj = num1 < num2 && num1 < num3? (document.getElementById("resultado").innerText = `El número menor es el: ${num1}`) 
+    :num2 < num1 && num2 < num3? (document.getElementById("resultado").innerText = `El número menor es el: ${num2}`)
+    :(document.getElementById("resultado").innerText = `El número menor es el: ${num3}`);
 
     num1 == num2 || num1 == num3 ? (document.getElementById("igual").innerText = `El número que se repite es el: ${num1}`)
     : num2 == num1 || num2 == num3? (document.getElementById("igual").innerText = `El número que se repite es el: ${num2}`)
     : num3 == num1 || num3 == num2? (document.getElementById("igual").innerText = `El número que se repite es el: ${num3}`)
     : (document.getElementById("igual").innerText = `No hay numeros repetidos`);
-       
-
-
   };
